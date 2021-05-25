769f127589a58abba9a5707c547b0cb3e44fdb7c
Ellis <352671309@qq.com>
2021-05-25 15:43:33 +0800

    Update auth-content.tsx

--- src/context/auth-content.tsx
+++ src/context/auth-content.tsx
@@ -1,6 +1,19 @@
+import React from "react";
 import { createContext, useState, useContext, ReactNode } from "react";
-import { User } from "../project-list/search-panel";
+import { User } from "../screens/project-list/search-panel";
 import * as auth from "auth-provider";
+import { http } from "../utils/http";
+import { useMount } from "../utils";
+
+const bootstrapUser = async () => {
+  let user = null;
+  const token = auth.getToken();
+  if (token) {
+    const data = await http("me", { token });
+    user = data.user;
+  }
+  return user;
+};
 
 interface IAuthForm {
   username: string,
@@ -11,18 +24,20 @@ export const AuthContext = createContext<{
   user: User | null,
   login: (form: IAuthForm) => Promise<void>
   register: (form: IAuthForm) => Promise<void>
-  logout: (form: IAuthForm) => Promise<void>
+  logout: () => Promise<void>
 
 } | undefined>(undefined);
 AuthContext.displayName = "AuthContext";
 
-export const AuthProvider = ({children}: {children: ReactNode}) => {
+export const AuthProvider = ({ children }: { children: ReactNode }) => {
   const [user, setUser] = useState<User | null>(null);
   // point free
   const login = (form: IAuthForm) => auth.login(form).then(setUser);
   const register = (form: IAuthForm) => auth.register(form).then(setUser);
   const logout = () => auth.logout().then(() => setUser(null));
-  // @ts-ignore
+  useMount(() => {
+    bootstrapUser().then(setUser);
+  });
   return <AuthContext.Provider children={children} value={{ user, login, register, logout }} />;
 };
 
@@ -31,5 +46,5 @@ export const useAuth = () => {
   if (!context) {
     throw new Error("useAuth 必须在AuthProvider中使用");
   }
-  return context
+  return context;
 };
