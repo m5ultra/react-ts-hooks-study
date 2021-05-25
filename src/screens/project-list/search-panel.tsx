696dd51d123875ac85deb6741d9db1185022123a
Dendi <352671309@qq.com>
2021-05-25 23:52:34 +0800

    chore: 替换search-panel部分组件

--- src/screens/project-list/search-panel.tsx
+++ src/screens/project-list/search-panel.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Input, Form, Select, Button } from "antd";
-import { useHttp } from "../../utils/http";
 import { useAuth } from "../../context/auth-content";
 
 export interface User {
@@ -24,41 +23,45 @@ interface SearchPanelProps {
 export const SearchPanel = ({ users, param, setParam }: SearchPanelProps) => {
   const { logout } = useAuth();
   return (
-    <Form layout={"inline"}>
-      {/*setParam(Object.assign({}, param, {name:evt.target.value}))*/}
-      <Form.Item>
-        <Button type={"primary"} onClick={logout}>登出</Button>
-      </Form.Item>
-      <Form.Item>
-        <Input
-          type="text"
-          value={param.name}
-          onChange={(evt) =>
-            setParam({
-              ...param,
-              name: evt.target.value
-            })
-          }
-        />
-      </Form.Item>
-      <Form.Item>
-        <Select
-          value={param.personId}
-          onChange={(val) =>
-            setParam({
-              ...param,
-              personId: val
-            })
-          }
-        >
-          <Select.Option value={""}>负责人</Select.Option>
-          {users.map((user) => (
-            <Select.Option key={user.id} value={user.id}>
-              {user.name}
-            </Select.Option>
-          ))}
-        </Select>
-      </Form.Item>
-    </Form>
+    <>
+      <div style={{ display: "block" }}>
+        <Button type={"primary"} onClick={logout}>
+          登出
+        </Button>
+      </div>
+      <Form layout={"inline"}>
+        {/*setParam(Object.assign({}, param, {name:evt.target.value}))*/}
+        <Form.Item>
+          <Input
+            type="text"
+            value={param.name}
+            onChange={(evt) =>
+              setParam({
+                ...param,
+                name: evt.target.value,
+              })
+            }
+          />
+        </Form.Item>
+        <Form.Item>
+          <Select
+            value={param.personId}
+            onChange={(val) =>
+              setParam({
+                ...param,
+                personId: val,
+              })
+            }
+          >
+            <Select.Option value={""}>负责人</Select.Option>
+            {users.map((user) => (
+              <Select.Option key={user.id} value={user.id}>
+                {user.name}
+              </Select.Option>
+            ))}
+          </Select>
+        </Form.Item>
+      </Form>
+    </>
   );
 };
