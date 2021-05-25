bf1bdfb147107add9b31c0b2f5d4d467cf7002d7
Ellis <352671309@qq.com>
2021-05-25 16:22:21 +0800

    Update auth-provider.ts

--- src/auth-provider.ts
+++ src/auth-provider.ts
@@ -4,7 +4,7 @@ import { User } from "screens/project-list/search-panel";
 
 const localStorageKey = "__auth_provider_token__";
 const apiUrl = process.env.REACT_APP_API_URL;
-export const getToken = () => window.localStorage.getItem("localStorageKey");
+export const getToken = () => window.localStorage.getItem(localStorageKey);
 
 export const handleUserResponse = ({ user }: { user: User }) => {
   window.localStorage.setItem(localStorageKey, user.token || "");
