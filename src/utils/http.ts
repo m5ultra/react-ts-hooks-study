0221b8d6378917b58ff5a55697521921574dab09
Ellis <352671309@qq.com>
2021-05-25 17:33:50 +0800

    Update http.ts

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -83,3 +83,37 @@ const xiaoMing: Person = { name: "xiaoming" };
 // 区别：联合类型 和 交叉类型无法 interface 无法替代type
 // interface 也无法实现 Utility Type
 
+type Person2 = {
+  name: string,
+  age: number
+}
+
+type Person3 = {}
+// Person2可以把某个类型中的类型变为可选类型
+const xiaoMing2: Partial<Person2> = { name: "xiaoming" };
+
+// Omit<类型1, 某个属性> 从类型1中去除某个属性 可以一次去除多个
+
+const shenMiRen: Omit<Person2, "name"> = { age: 88 };
+const shenMiRen2: Omit<Person2, "name" | "age"> = { age: 88, name: "Dendi", gender: "0" };
+
+const P3: Person3 = { name: "Dendi" }
+type PersonKeys = keyof Person2
+type PersonOnlyName = Pick<Person2, "name">
+
+// Partial 的实现
+type Partial<T> = {
+  [P in keyof T]?: T[P]
+}
+
+// Pick 实现
+// K extends keyof T：  keyof T 会返回T中所有类型的联合类型 extends 关键字约束K 继承 keyof T的联合类型
+
+type Pick<T, K extends keyof T> = {
+  [P in K]: T[P];
+}
+
+// Omit
+// type Omit<T, K extends keyof any> = Pick<T, Exclude<keyof T, K>>;
+
+// type Exclude<T, U> = T extends U ? never : T;
