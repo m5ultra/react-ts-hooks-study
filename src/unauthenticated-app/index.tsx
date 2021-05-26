22bfda272992c88bae8fece2fdcc4a737e8eb13e
Dendi <352671309@qq.com>
2021-05-26 08:49:40 +0800

    feat: 安装@emotion/react @emotion/styled

--- src/unauthenticated-app/index.tsx
+++ src/unauthenticated-app/index.tsx
@@ -2,16 +2,21 @@ import React, { useState } from "react";
 import { Button, Card } from "antd";
 import { RegisterScreen } from "./register";
 import { LoginScreen } from "./login";
-
+import styled from "@emotion/styled";
 export const UnauthenticatedApp = () => {
   const [isRegister, setIsRegister] = useState(false);
 
   return (
-    <div style={{ display: "flex", justifyContent: "center" }}>
+    <Container>
       <Card>
         {isRegister ? <LoginScreen /> : <RegisterScreen />}
         <Button type={"default"} onClick={() => setIsRegister(!isRegister)}>切换到{isRegister ? "注册" : "登录"}</Button>
       </Card>
-</div>
+    </Container>
   );
 };
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+`;
