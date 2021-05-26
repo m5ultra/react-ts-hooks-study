2652aa68b041621f0856150db8f0de2f8a123247
Dendi <352671309@qq.com>
2021-05-26 17:16:29 +0800

    styl: 修改登录与未登录逻辑以及样式

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import { ProjectListScreen } from './screens/project-list'
 import styled from '@emotion/styled'
-import { Button } from 'antd'
+import { Button, Dropdown, Menu } from 'antd'
 import { useAuth } from './context/auth-content'
 import { Row } from './components/lib'
 import { ReactComponent as SoftwareLogo } from 'assets/software-logo.svg'
 
 export const AuthenticatedApp = () => {
-  const { logout } = useAuth()
+  const { logout, user } = useAuth()
   return (
     <Container>
       <Header between={true}>
@@ -16,9 +16,26 @@ export const AuthenticatedApp = () => {
           <h2>项目</h2>
           <h2>用户</h2>
         </HeaderLeft>
-        <Button type={'primary'} onClick={logout}>
-          登出
-        </Button>
+        <HeaderRight>
+          {/*<Button type={'primary'} onClick={logout}>*/}
+          {/*  登出*/}
+          {/*</Button>*/}
+          <Dropdown
+            overlay={
+              <Menu>
+                <Menu.Item key={'logout'}>
+                  <a href="" onClick={logout}>
+                    登出
+                  </a>
+                </Menu.Item>
+              </Menu>
+            }
+          >
+            <a href="" onClick={(e) => e.preventDefault()}>
+              Hi, {user?.name}
+            </a>
+          </Dropdown>
+        </HeaderRight>
       </Header>
       {/*<Nav>nav</Nav>*/}
       <Main>
@@ -55,6 +72,7 @@ const Header = styled(Row)`
 `
 
 const HeaderLeft = styled(Row)``
+const HeaderRight = styled(Row)``
 const Main = styled.main`
   grid-area: main;
 `
