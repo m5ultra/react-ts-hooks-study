378b1e2032e128ffb7ea6b0092a5e86db1a25b06
Dendi <352671309@qq.com>
2021-05-27 14:19:30 +0800

    feat: 增加确认密码

--- src/unauthenticated-app/register.tsx
+++ src/unauthenticated-app/register.tsx
@@ -9,7 +9,10 @@ export const RegisterScreen = ({ onError }: { onError: (err: Error) => void }) =
   const { user, register } = useAuth()
 
   // HTMLFormElement extends Element
-  const handleSubmit = (values: { username: string; password: string }) => {
+  const handleSubmit = ({ c_password, ...values }: { username: string; password: string; c_password: string }) => {
+    if (c_password !== values.password) {
+      onError(new Error('请确认两次输入的密码是否相同'))
+    }
     register(values).catch((e) => {
       onError(e)
     })
@@ -23,6 +26,9 @@ export const RegisterScreen = ({ onError }: { onError: (err: Error) => void }) =
       </Form.Item>
       <Form.Item name={'password'} htmlFor={'password'} rules={[{ required: true, message: '请输入密码' }]}>
         <Input placeholder={'密码'} type="password" id={'password'} />
+      </Form.Item>
+      <Form.Item name={'c_password'} htmlFor={'c_password'} rules={[{ required: true, message: '请确认密码' }]}>
+        <Input placeholder={'密码'} type="password" id={'c_password'} />
       </Form.Item>
       <Form.Item>
         <LongButton type={'primary'} htmlType={'submit'}>
