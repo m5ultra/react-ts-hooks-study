a65ca0b6725c200d168c5bd6b37db49811c9396c
Dendi <352671309@qq.com>
2021-05-31 22:06:28 +0800

    fix: study 7-4

--- src/components/lib.tsx
+++ src/components/lib.tsx
@@ -1,4 +1,7 @@
-import styled from '@emotion/styled'
+import React from "react";
+import { DevTools } from "jira-dev-tool";
+import styled from "@emotion/styled";
+import { Spin, Typography } from "antd";
 
 export const Row = styled.div<{
   gap?: number | boolean
@@ -7,12 +10,30 @@ export const Row = styled.div<{
 }>`
   display: flex;
   align-items: center;
-  justify-content: ${(props) => (props.between ? 'space-between' : undefined)};
-  margin-bottom: ${(props) => props.marginBottom + 'rem'};
+  justify-content: ${(props) => (props.between ? "space-between" : undefined)};
+  margin-bottom: ${(props) => props.marginBottom + "rem"};
 
   > * {
     margin-top: 0 !important;
     margin-bottom: 0 !important;
-    margin-right: ${(props) => (typeof props.gap === 'number' ? props.gap + 'rem' : props.gap ? '2rem' : undefined)};
+    margin-right: ${(props) => (typeof props.gap === "number" ? props.gap + "rem" : props.gap ? "2rem" : undefined)};
   }
-`
+`;
+
+export const FullPage = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const FullPageLoading = () =>
+  <FullPage>
+    <Spin size={"large"} />
+  </FullPage>;
+
+
+export const FullPageError = ({ error }: { error: Error | null }) => <FullPage>
+  <DevTools />
+  <Typography.Text type={"danger"}>{error?.message}</Typography.Text>
+</FullPage>;
--- src/context/auth-content.tsx
+++ src/context/auth-content.tsx
@@ -4,6 +4,8 @@ import { User } from "../screens/project-list/search-panel";
 import * as auth from "auth-provider";
 import { http } from "../utils/http";
 import { useMount } from "../utils";
+import { useAsync } from "../utils/use-async";
+import { FullPage, FullPageError, FullPageLoading } from "../components/lib";
 
 const bootstrapUser = async () => {
   let user = null;
@@ -16,28 +18,35 @@ const bootstrapUser = async () => {
 };
 
 interface IAuthForm {
-  username: string,
+  username: string
   password: string
 }
 
-export const AuthContext = createContext<{
-  user: User | null,
+export const AuthContext = createContext<| {
+  user: User | null
   login: (form: IAuthForm) => Promise<void>
   register: (form: IAuthForm) => Promise<void>
   logout: () => Promise<void>
-
-} | undefined>(undefined);
+}
+  | undefined>(undefined);
 AuthContext.displayName = "AuthContext";
 
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
-  const [user, setUser] = useState<User | null>(null);
+  const { data: user, error, isLoading, isIdle, isError, run, setData: setUser } = useAsync<User | null>();
   // point free
   const login = (form: IAuthForm) => auth.login(form).then(setUser);
   const register = (form: IAuthForm) => auth.register(form).then(setUser);
   const logout = () => auth.logout().then(() => setUser(null));
   useMount(() => {
-    bootstrapUser().then(setUser);
+    run(bootstrapUser());
   });
+  if (isIdle || isLoading) {
+    return <FullPageLoading />;
+  }
+
+  if (isError) {
+    return <FullPageError error={error} />;
+  }
   return <AuthContext.Provider children={children} value={{ user, login, register, logout }} />;
 };
 
@@ -47,4 +56,4 @@ export const useAuth = () => {
     throw new Error("useAuth 必须在AuthProvider中使用");
   }
   return context;
-};
+}
